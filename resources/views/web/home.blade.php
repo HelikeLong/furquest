1a9e5db63e7001b789f232018c8c2368bbbde267
Helike Long <pedro.aquino.idealtrends@gmail.com>
2019-12-02 18:00:05 -0300

    Main focus | Registrations (dev)

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -4,11 +4,13 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 -->
-<html>
+<html lang="pt-br">
 <head>
-    <title>Spectral by HTML5 UP</title>
+    <title>FurQuest | Evento Furry de Caçada ao Tesouro ARG</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+
+    <link href="https://fonts.googleapis.com/css?family=Atma:400,500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ vAsset('web/css/main.css') }}" />
     <noscript><link rel="stylesheet" href="{{ vAsset('web/css/noscript.css') }}" /></noscript>
     <link rel="stylesheet" href="{{ vAsset('web/css/custom.css') }}" />
@@ -66,48 +68,45 @@
                 <img src="{{ vAsset('images/logo-negative-typo.png') }}" alt="FurQuest" width="250px">
             </h2>
             <p>Evento Furry de Caçada ao Tesouro em ARG.</p>
-        </div>
-        <a href="#one" class="more scrolly">Conheça</a>
-    </section>
-
-    <!-- One -->
-    <section id="one" class="wrapper style1 special">
-        <div class="inner">
-            <header class="major">
-                <h2>Arcu aliquet vel lobortis ata nisl<br />
-                    eget augue amet aliquet nisl cep donec</h2>
-                <p>Aliquam ut ex ut augue consectetur interdum. Donec amet imperdiet eleifend<br />
-                    fringilla tincidunt. Nullam dui leo Aenean mi ligula, rhoncus ullamcorper.</p>
-            </header>
-            <ul class="icons major">
-                <li><span class="icon fa-gem major style1"><span class="label">Lorem</span></span></li>
-                <li><span class="icon fa-heart major style2"><span class="label">Ipsum</span></span></li>
-                <li><span class="icon solid fa-code major style3"><span class="label">Dolor</span></span></li>
+            <ul class="actions special">
+                <li><a href="https://www.kickante.com.br/campanhas/furquest-pirates-n-gears" class="button primary cta">Quero Participar!</a></li>
             </ul>
         </div>
+        <a href="#one" class="more scrolly">Conheça</a>
     </section>
 
     <!-- Two -->
     <section id="two" class="wrapper alt style2">
         <section class="spotlight">
-            <div class="image"><img src="images/pic01.jpg" alt="" /></div><div class="content">
-                <h2>Magna primis lobortis<br />
-                    sed ullamcorper</h2>
-                <p>Aliquam ut ex ut augue consectetur interdum. Donec hendrerit imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.</p>
+            <div class="image">
+
+            </div>
+            <div class="content">
+                <h2>Venha experimentar a nostalgia<br />
+                    de uma caçada ao tesouro</h2>
+                <p>Com um toque moderno.<br/>O ARG, ou "Jogo de Realidade Alternativa" em português, irá lhe proporcionar uma experiência completamente nova!</p>
             </div>
         </section>
         <section class="spotlight">
-            <div class="image"><img src="images/pic02.jpg" alt="" /></div><div class="content">
-                <h2>Tortor dolore feugiat<br />
-                    elementum magna</h2>
-                <p>Aliquam ut ex ut augue consectetur interdum. Donec hendrerit imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.</p>
+            <div class="image">
+
+            </div>
+            <div class="content">
+                <h2>Desevende enigmas com seus<br />
+                    amigos e conheça mais sobre SP</h2>
+                <p>Os participantes serão separados em trios, chamados de guildas, para desenvendar os enigmas da caçada,
+                    com o auxílio de dicas, enquanto conhecem e aprendem mais sobre os locais onde o evento será realizado. </p>
             </div>
         </section>
         <section class="spotlight">
-            <div class="image"><img src="images/pic03.jpg" alt="" /></div><div class="content">
-                <h2>Augue eleifend aliquet<br />
-                    sed condimentum</h2>
-                <p>Aliquam ut ex ut augue consectetur interdum. Donec hendrerit imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.</p>
+            <div class="image">
+
+            </div>
+            <div class="content">
+                <h2>E concorra a mais de R$1000<br />
+                    em prêmios exclusivos</h2>
+                <p>Nossos baús estaram recheados de prêmios como commissions com grandes artistas, incríveis prints
+                    feitas especialmente para o evento, pelúcias, canecas e muito outros brindes exclusivos!</p>
             </div>
         </section>
     </section>
@@ -122,27 +121,41 @@
             </header>
             <ul class="features">
                 <li class="icon fa-paper-plane">
-                    <h3>Arcu accumsan</h3>
+                    <h3>Aventureiro - R$40</h3>
                     <p>Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem tincidunt nullam amet leo Aenean ligula consequat consequat.</p>
                 </li>
                 <li class="icon solid fa-laptop">
-                    <h3>Ac Augue Eget</h3>
+                    <h3>Super Aventureiro - R$55</h3>
                     <p>Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem tincidunt nullam amet leo Aenean ligula consequat consequat.</p>
                 </li>
                 <li class="icon solid fa-code">
-                    <h3>Mus Scelerisque</h3>
+                    <h3>Guilda - R$150</h3>
                     <p>Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem tincidunt nullam amet leo Aenean ligula consequat consequat.</p>
                 </li>
                 <li class="icon solid fa-headphones-alt">
-                    <h3>Mauris Imperdiet</h3>
+                    <h3>Super Guilda - R$195</h3>
                     <p>Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem tincidunt nullam amet leo Aenean ligula consequat consequat.</p>
                 </li>
+            </ul>
+
+            <header class="major">
+                <h2>Accumsan mus tortor nunc aliquet</h2>
+                <p>Aliquam ut ex ut augue consectetur interdum. Donec amet imperdiet eleifend<br />
+                    fringilla tincidunt. Nullam dui leo Aenean mi ligula, rhoncus ullamcorper.</p>
+            </header>
+            <ul class="features">
+                <li style="display:none;"></li>
+                <li style="display:none;"></li>
+                <li style="display:none;"></li>
+                <li style="display:none;"></li>
                 <li class="icon fa-heart">
-                    <h3>Aenean Primis</h3>
+                    <h3>NPC - R$15</h3>
                     <p>Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem tincidunt nullam amet leo Aenean ligula consequat consequat.</p>
+
+                    <a href="https://www.kickante.com.br/campanhas/furquest-pirates-n-gears" class="button primary cta">Quero esse!</a>
                 </li>
                 <li class="icon fa-flag">
-                    <h3>Tortor Ut</h3>
+                    <h3>Super NPC - R$30</h3>
                     <p>Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem tincidunt nullam amet leo Aenean ligula consequat consequat.</p>
                 </li>
             </ul>
@@ -166,15 +179,16 @@
     <!-- Footer -->
     <footer id="footer">
         <ul class="icons">
-            <li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
-            <li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
-            <li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-            <li><a href="#" class="icon brands fa-dribbble"><span class="label">Dribbble</span></a></li>
-            <li><a href="#" class="icon solid fa-envelope"><span class="label">Email</span></a></li>
+            <li><a href="https://fb.com/FurQuestBR" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+{{--            <li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>--}}
+            <li><a href="https://t.me/FurQuest" class="icon brands fa-telegram"><span class="label">Telegram</span></a></li>
+            <li><a href="https://youtube.com/watch?v=XMUVxtMb2LA" class="icon brands fa-youtube"><span class="label">Youtube</span></a></li>
+            <li><a href="https://www.kickante.com.br/campanhas/furquest-pirates-n-gears" class="icon solid fa-globe"><span class="label">Kickante</span></a></li>
         </ul>
         <ul class="copyright">
-            <li>&copy; Untitled</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+            <li>&copy; FurQuest</li><li>Um projeto <a href="https://crystalhorns.com/">CrystalHorns</a></li>
         </ul>
+        <sup style="opacity: .5;"><sup><sup>Design: <a href="http://html5up.net">HTML5 UP</a></sup></sup></sup>
     </footer>
 
 </div>
