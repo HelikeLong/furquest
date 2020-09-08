df9e6f6dbc9922922cfab30c86ed5b288ea269ab
Helike Long <pedro.aquino.idealtrends@gmail.com>
2020-09-08 14:24:31 -0300

    Fix seeds

--- database/seeds/TipsTableSeeder.php
+++ database/seeds/TipsTableSeeder.php
@@ -20,28 +20,32 @@ class TipsTableSeeder extends Seeder
                 'step_id' => 1,
                 'content' => 'iuhsdjg siu giusg huis dviusfaudg oa wgu iug agi ari gag is aofhlsadj ls iuasyi s gpoisyfp slfj siieuftoaf laweutf gslajfbf piaiuef fofas flwjfliueyf lwjelu ',
                 'map' => 0,
-                'delay' => 600
+                'delay' => 600,
+                'character' => 'spocky'
             ),
             array (
                 'quest_id' => 1,
                 'step_id' => 1,
                 'content' => 'liug duyu poasdygsdgf sfsafuef asfsefpiu asipug iaefu gaç fpuate faleu foaet fagleftoauf hdvfjvsdkufyt oaefgausf leftie',
                 'map' => 0,
-                'delay' => 600
+                'delay' => 600,
+                'character' => 'spocky'
             ),
             array (
                 'quest_id' => 1,
                 'step_id' => 1,
                 'content' => 'eruig eru rgh',
                 'map' => 0,
-                'delay' => 300
+                'delay' => 300,
+                'character' => 'spocky'
             ),
             array (
                 'quest_id' => 1,
                 'step_id' => 1,
                 'content' => 'oijjgdflkdfgldfkgj',
                 'map' => 1,
-                'delay' => 300
+                'delay' => 300,
+                'character' => 'spocky'
             ),
 
             array (
@@ -49,28 +53,32 @@ class TipsTableSeeder extends Seeder
                 'step_id' => 2,
                 'content' => '2 - iuhsdjg siu giusg huis dviusfaudg oa wgu iug agi ari gag is aofhlsadj ls iuasyi s gpoisyfp slfj siieuftoaf laweutf gslajfbf piaiuef fofas flwjfliueyf lwjelu ',
                 'map' => 0,
-                'delay' => 600
+                'delay' => 600,
+                'character' => 'spocky'
             ),
             array (
                 'quest_id' => 1,
                 'step_id' => 2,
                 'content' => '2 - liug duyu poasdygsdgf sfsafuef asfsefpiu asipug iaefu gaç fpuate faleu foaet fagleftoauf hdvfjvsdkufyt oaefgausf leftie',
                 'map' => 0,
-                'delay' => 600
+                'delay' => 600,
+                'character' => 'spocky'
             ),
             array (
                 'quest_id' => 1,
                 'step_id' => 2,
                 'content' => '2 - eruig eru rgh',
                 'map' => 0,
-                'delay' => 300
+                'delay' => 300,
+                'character' => 'spocky'
             ),
             array (
                 'quest_id' => 1,
                 'step_id' => 2,
                 'content' => '2 - oijjgdflkdfgldfkgj',
                 'map' => 1,
-                'delay' => 300
+                'delay' => 300,
+                'character' => 'spocky'
             ),
 
             array (
@@ -78,28 +86,32 @@ class TipsTableSeeder extends Seeder
                 'step_id' => 3,
                 'content' => '3 - iuhsdjg siu giusg huis dviusfaudg oa wgu iug agi ari gag is aofhlsadj ls iuasyi s gpoisyfp slfj siieuftoaf laweutf gslajfbf piaiuef fofas flwjfliueyf lwjelu ',
                 'map' => 0,
-                'delay' => 600
+                'delay' => 600,
+                'character' => 'spocky'
             ),
             array (
                 'quest_id' => 1,
                 'step_id' => 3,
                 'content' => '3 - liug duyu poasdygsdgf sfsafuef asfsefpiu asipug iaefu gaç fpuate faleu foaet fagleftoauf hdvfjvsdkufyt oaefgausf leftie',
                 'map' => 0,
-                'delay' => 600
+                'delay' => 600,
+                'character' => 'spocky'
             ),
             array (
                 'quest_id' => 1,
                 'step_id' => 3,
                 'content' => '3 - eruig eru rgh',
                 'map' => 0,
-                'delay' => 300
+                'delay' => 300,
+                'character' => 'spocky'
             ),
             array (
                 'quest_id' => 1,
                 'step_id' => 3,
                 'content' => '3 - oijjgdflkdfgldfkgj',
                 'map' => 1,
-                'delay' => 300
+                'delay' => 300,
+                'character' => 'spocky'
             ),
 
             array (
@@ -107,28 +119,32 @@ class TipsTableSeeder extends Seeder
                 'step_id' => 3,
                 'content' => '3 - iuhsdjg siu giusg huis dviusfaudg oa wgu iug agi ari gag is aofhlsadj ls iuasyi s gpoisyfp slfj siieuftoaf laweutf gslajfbf piaiuef fofas flwjfliueyf lwjelu ',
                 'map' => 0,
-                'delay' => 600
+                'delay' => 600,
+                'character' => 'spocky'
             ),
             array (
                 'quest_id' => 1,
                 'step_id' => 3,
                 'content' => '3 - liug duyu poasdygsdgf sfsafuef asfsefpiu asipug iaefu gaç fpuate faleu foaet fagleftoauf hdvfjvsdkufyt oaefgausf leftie',
                 'map' => 0,
-                'delay' => 600
+                'delay' => 600,
+                'character' => 'spocky'
             ),
             array (
                 'quest_id' => 1,
                 'step_id' => 3,
                 'content' => '3 - eruig eru rgh',
                 'map' => 0,
-                'delay' => 300
+                'delay' => 300,
+                'character' => 'spocky'
             ),
             array (
                 'quest_id' => 1,
                 'step_id' => 3,
                 'content' => '3 - oijjgdflkdfgldfkgj',
                 'map' => 1,
-                'delay' => 300
+                'delay' => 300,
+                'character' => 'spocky'
             ),
 
             array (
@@ -136,28 +152,32 @@ class TipsTableSeeder extends Seeder
                 'step_id' => 4,
                 'content' => '4 - iuhsdjg siu giusg huis dviusfaudg oa wgu iug agi ari gag is aofhlsadj ls iuasyi s gpoisyfp slfj siieuftoaf laweutf gslajfbf piaiuef fofas flwjfliueyf lwjelu ',
                 'map' => 0,
-                'delay' => 600
+                'delay' => 600,
+                'character' => 'spocky'
             ),
             array (
                 'quest_id' => 1,
                 'step_id' => 4,
                 'content' => '4 - liug duyu poasdygsdgf sfsafuef asfsefpiu asipug iaefu gaç fpuate faleu foaet fagleftoauf hdvfjvsdkufyt oaefgausf leftie',
                 'map' => 0,
-                'delay' => 600
+                'delay' => 600,
+                'character' => 'spocky'
             ),
             array (
                 'quest_id' => 1,
                 'step_id' => 4,
                 'content' => '4 - eruig eru rgh',
                 'map' => 0,
-                'delay' => 300
+                'delay' => 300,
+                'character' => 'spocky'
             ),
             array (
                 'quest_id' => 1,
                 'step_id' => 4,
                 'content' => '4 - oijjgdflkdfgldfkgj',
                 'map' => 1,
-                'delay' => 300
+                'delay' => 300,
+                'character' => 'spocky'
             ),
 
             array (
@@ -165,28 +185,32 @@ class TipsTableSeeder extends Seeder
                 'step_id' => 5,
                 'content' => '5 - iuhsdjg siu giusg huis dviusfaudg oa wgu iug agi ari gag is aofhlsadj ls iuasyi s gpoisyfp slfj siieuftoaf laweutf gslajfbf piaiuef fofas flwjfliueyf lwjelu ',
                 'map' => 0,
-                'delay' => 600
+                'delay' => 600,
+                'character' => 'spocky'
             ),
             array (
                 'quest_id' => 1,
                 'step_id' => 5,
                 'content' => '5 - liug duyu poasdygsdgf sfsafuef asfsefpiu asipug iaefu gaç fpuate faleu foaet fagleftoauf hdvfjvsdkufyt oaefgausf leftie',
                 'map' => 0,
-                'delay' => 600
+                'delay' => 600,
+                'character' => 'spocky'
             ),
             array (
                 'quest_id' => 1,
                 'step_id' => 5,
                 'content' => '5 - eruig eru rgh',
                 'map' => 0,
-                'delay' => 300
+                'delay' => 300,
+                'character' => 'spocky'
             ),
             array (
                 'quest_id' => 1,
                 'step_id' => 5,
                 'content' => '5 - oijjgdflkdfgldfkgj',
                 'map' => 1,
-                'delay' => 300
+                'delay' => 300,
+                'character' => 'spocky'
             ),
         ));
     }
